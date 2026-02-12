89c5d1932013d5941293a3fbe00d80eb880bc06e
aylin <4ylingunes@gmail.com>
2026-02-12 15:05:54 +0100

    2 is ok

--- js/task-1.js
+++ js/task-1.js
@@ -6,7 +6,7 @@ function makeTransaction(quantity, pricePerDroid) {
 }
 
 
-function runHomework() {
+function runHomework1() {
 
     console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
     console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
